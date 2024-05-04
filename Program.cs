41a75490af6a849aedf75724d3758ea9713834ac
Douglas Nunes <paulodnunes2016@gmail.com>
2024-05-04 20:20:40 -0300

    Xadrez do zero

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Tabuleiro;
+
+namespace xadez_console
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3,4);
+
+            Console.WriteLine(" Posicao: " + P);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
